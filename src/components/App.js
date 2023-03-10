3b56cd59408162c8c73415b7f84388d794bd77f0
Henry <henrysungyun@gmail.com>
2023-03-10 13:58:04 -0800

    deploying

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
-import LogIn from "./LogIn";
-import NewAccount from "./NewAccount";
-import HomePage from "./HomePage";
-import ArtistCollection from "./ArtistCollection";
-import OwnWork from "./OwnWork";
+import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import LogIn from './LogIn';
+import NewAccount from './NewAccount';
+import HomePage from './HomePage';
+import ArtistCollection from './ArtistCollection';
+import OwnWork from './OwnWork';
 
 const artistAPI = 'http://localhost:3000/artists';
 
@@ -18,58 +18,57 @@ function App() {
     fetch(artistAPI)
       .then(res => res.json())
       .then(artistData => {
-        // console.log(artistData);
         setArtists(artistData);
       })
       .catch(error => console.log(error));
   }, []);
 
   return (
-  <>
-   <div className="App ui">
-    <Switch>
-      <Route exact path="/">
-        <LogIn 
-          setCurrentUser={setCurrentUser} 
-          artists={artists} 
-          username={username} 
-          setUsername={setUsername} 
-          password={password} 
-          setPassword={setPassword} 
-        />
-      </Route>
-      <Route exact path="/new-account">
-        <NewAccount 
-          API={artistAPI}
-          artists={artists}
-          setArtists={setArtists}
-          setUsername={setUsername} 
-          setPassword={setPassword} 
-        />
-      </Route>
-      <Route exact path="/home">
-        <HomePage 
-          currentUser={currentUser}
-          artists={artists}
-          setArtists={setArtists}
-        />
-      </Route>
-      <Route exact path="/artist-collection">
-        <ArtistCollection 
-          currentUser={currentUser} 
-        />
-      </Route>
-      <Route exact path="/creations">
-        <OwnWork 
-          currentUser={currentUser} 
-      />
-      </Route>
-      <Route path="*">
-        <h1>404 not found</h1>
-      </Route>
-    </Switch>
-   </div>
-   </>
+    <>
+      <div className="App ui">
+        <Switch>
+          <Route exact path="/">
+            <LogIn 
+              setCurrentUser={setCurrentUser} 
+              artists={artists} 
+              username={username} 
+              setUsername={setUsername} 
+              password={password} 
+              setPassword={setPassword} 
+            />
+          </Route>
+          <Route exact path="/new-account">
+            <NewAccount 
+              API={artistAPI}
+              artists={artists}
+              setArtists={setArtists}
+              setUsername={setUsername} 
+              setPassword={setPassword} 
+            />
+          </Route>
+          <Route exact path="/home">
+            <HomePage 
+              currentUser={currentUser}
+              artists={artists}
+              setArtists={setArtists}
+            />
+          </Route>
+          <Route exact path="/artist-collection">
+            <ArtistCollection 
+              currentUser={currentUser} 
+            />
+          </Route>
+          <Route exact path="/creations">
+            <OwnWork 
+              currentUser={currentUser} 
+            />
+          </Route>
+          <Route path="*">
+            <h1>404 not found</h1>
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 }
 
